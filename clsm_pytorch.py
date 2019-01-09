7fcafe8b841ce1666ce772a8e9bd4a0a64b1c7bd
Moin Nadeem <mnadeem@mit.edu>
2019-01-09 10:44:53 -0500

    faste data loader

--- clsm_pytorch.py
+++ clsm_pytorch.py
@@ -16,6 +16,7 @@ from multiprocessing import cpu_count
 from sys import argv
 
 import joblib
+import pytorch_utils as putils
 import nltk
 import numpy as np
 import torch
@@ -43,6 +44,7 @@ nltk.data.path.append('/usr/users/mnadeem/nltk_data/')
 def parse_args():
     parser = argparse.ArgumentParser(description='Learning the optimal convolution for network.')
     parser.add_argument("--batch-size", type=int, help="Number of queries per batch.", default=1)
+    parser.add_argument("--model", help="Loading a pretrained model.", default=None)
     parser.add_argument("--data-batch-size", type=int, help="Number of examples per query.", default=8)
     parser.add_argument("--learning-rate", type=float, help="Learning rate for model.", default=1e-3)
     parser.add_argument("--epochs", type=int, help="Number of epochs to learn for.", default=3)
@@ -50,46 +52,41 @@ def parse_args():
     parser.add_argument("--sparse-evidences", default=False, action="store_true")
     return parser.parse_args()
 
-def save_checkpoint(state, is_best, filename='/output/checkpoint.pth.tar'):
-    """Save checkpoint if a new best is achieved"""
-    if is_best:
-        print ("=> Saving a new best")
-        torch.save(state, filename)  # save checkpoint
-    else:
-        print ("=> Validation Accuracy did not improve")
-
 def run(args, train, sparse_evidences, claims_dict):
     BATCH_SIZE = args.batch_size
     LEARNING_RATE = args.learning_rate
     DATA_BATCH_SIZE = args.data_batch_size
     NUM_EPOCHS = args.epochs
+    MODEL = args.model
     
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
     logger = Logger('./logs/{}'.format(time.localtime()))
 
-    print("Created model...")
     model = cdssm.CDSSM()
-    model = model.cuda()
     model = model.to(device)
     if torch.cuda.device_count() > 0:
       print("Let's use", torch.cuda.device_count(), "GPU(s)!")
       model = nn.DataParallel(model)
+    print("Created model with {:,} parameters.".format(putils.count_parameters(model)))
+
+    if MODEL:
+      model.load_state_dict(torch.load(MODEL))
 
     print("Created dataset...")
     train_size = int(len(train) * 0.8)
     #test = int(len(train) * 0.5)
     train_dataset = pytorch_data_loader.WikiDataset(train[:train_size], claims_dict, data_batch_size=DATA_BATCH_SIZE, sparse_evidences=sparse_evidences) 
     val_dataset = pytorch_data_loader.WikiDataset(train[train_size:], claims_dict, data_batch_size=DATA_BATCH_SIZE, sparse_evidences=sparse_evidences) 
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=5, shuffle=True, collate_fn=pytorch_data_loader.variable_collate)
-    val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=5, shuffle=True, collate_fn=pytorch_data_loader.variable_collate)
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=True, collate_fn=pytorch_data_loader.PadCollate())
+    val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=True, collate_fn=pytorch_data_loader.PadCollate())
 
     # Loss and optimizer
     criterion = torch.nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)
 
-    OUTPUT_FREQ = int((len(train_dataset)/BATCH_SIZE)*0.02) 
+    OUTPUT_FREQ = max(int((len(train_dataset)/BATCH_SIZE)*0.02), 20) 
     parameters = {"batch size": BATCH_SIZE, "epochs": NUM_EPOCHS, "learning rate": LEARNING_RATE, "optimizer": optimizer.__class__.__name__, "loss": criterion.__class__.__name__, "training size": train_size, "data batch size": DATA_BATCH_SIZE, "data": args.data, "sparse_evidences": args.sparse_evidences}
     exp_params = {}
     exp = Experiment("CLSM V2")
@@ -111,23 +108,19 @@ def run(args, train, sparse_evidences, claims_dict):
         mean_train_acc = 0.0
         train_running_loss = 0.0
         train_running_accuracy = 0.0
-        val_running_loss = 0.0
         val_running_accuracy = 0.0
         model.train()
 
         for train_batch_num, inputs in enumerate(train_dataloader):
             claims, evidences, labels = inputs  
 
-            claims = claims.to(device) 
-            evidences = evidences.to(device) 
-            claims = claims.cuda()
-            evidences = evidences.cuda()
-            labels = labels.to(device)
-            labels = labels.cuda()
+            #claims = claims.to(device).float()
+            #evidences = evidences.to(device).float()
+            #labels = labels.to(device)
 
             y_pred = model(claims, evidences)
 
-            y = (labels).float()
+            y = (labels)
             y_pred = y_pred.squeeze()
             y = y.squeeze()
             y = y.view(-1)
@@ -136,7 +129,7 @@ def run(args, train, sparse_evidences, claims_dict):
             loss = criterion(y_pred, y)
 
             predictions = torch.sigmoid(y_pred).round()
-            accuracy = (y==predictions).float()
+            accuracy = (y==predictions).cuda().float()
             accuracy = accuracy.mean()
             train_running_accuracy += accuracy.item()
             mean_train_acc += accuracy.item()
@@ -160,11 +153,14 @@ def run(args, train, sparse_evidences, claims_dict):
                 #    logger.histo_summary(tag+'/grad', value.grad.detach().cpu().numpy(), train_batch_num+1)
 
                 train_running_loss = 0.0
-                beginning_time = 0.0
+                beginning_time = time.time() 
                 train_running_accuracy = 0.0
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+
+            del loss
+            del accuracy
 
         print("Running validation...")
         model.eval()
@@ -172,42 +168,41 @@ def run(args, train, sparse_evidences, claims_dict):
             claims, evidences, labels = inputs  
 
             claims = claims.to(device) 
-            evidences = evidences.to(device) 
-            claims = claims.cuda()
-            evidences = evidences.cuda()
+            evidences = evidences.to(device).float()
+            claims = claims.cuda().float()
+            evidences = evidences.cuda().float()
             labels = labels.to(device)
             labels = labels.cuda()
 
             y_pred = model(claims, evidences)
 
-            y = (labels).float()
+            y = (labels)
             y_pred = y_pred.squeeze()
             y = y.squeeze()
             y = y.view(-1)
             y_pred = y_pred.view(-1)
 
             predictions = F.sigmoid(y_pred).round()
-            accuracy = (y==predictions).float().mean()
+            accuracy = (y==predictions).to(device)
+            accuracy = accuracy.float().mean()
             val_running_accuracy += accuracy.item()
-            val_running_loss += loss.item()
 
             if (val_batch_num % OUTPUT_FREQ)==0 and val_batch_num>0:
-                print("[{}:{}] loss: {}, accuracy: {}, recall: {}".format(epoch, val_batch_num / (len(val_dataset)/BATCH_SIZE), val_running_loss/OUTPUT_FREQ, val_running_accuracy/OUTPUT_FREQ, recall_score(y.detach().cpu().numpy(), predictions.detach().cpu().numpy())))
+                print("[{}:{}]  accuracy: {}, recall: {}".format(epoch, val_batch_num / (len(val_dataset)/BATCH_SIZE), val_running_accuracy/OUTPUT_FREQ, recall_score(y.detach().cpu().numpy(), predictions.detach().cpu().numpy())))
 
                 # 1. Log scalar values (scalar summary)
-                info = { 'val_loss': val_running_loss/OUTPUT_FREQ, 'val_accuracy': val_running_accuracy/OUTPUT_FREQ }
+                #info = { 'val_accuracy': val_running_accuracy/OUTPUT_FREQ }
 
-                for tag, value in info.items():
-                    exp.metric(tag, value, log=False)
-                    logger.scalar_summary(tag, value, val_batch_num+1)
+                #for tag, value in info.items():
+                #    exp.metric(tag, value, log=False)
+                #    logger.scalar_summary(tag, value, val_batch_num+1)
 
-                # 2. Log values and gradients of the parameters (histogram summary)
-                for tag, value in model.named_parameters():
-                    tag = tag.replace('.', '/')
-                    logger.histo_summary(tag, value.detach().cpu().numpy(), val_batch_num+1)
-                    logger.histo_summary(tag+'/grad', value.grad.detach().cpu().numpy(), val_batch_num+1)
+                ## 2. Log values and gradients of the parameters (histogram summary)
+                #for tag, value in model.named_parameters():
+                #    tag = tag.replace('.', '/')
+                #    logger.histo_summary(tag, value.detach().cpu().numpy(), val_batch_num+1)
+                #    logger.histo_summary(tag+'/grad', value.grad.detach().cpu().numpy(), val_batch_num+1)
 
-                val_running_loss = 0.0
                 val_running_accuracy = 0.0
 
         train_acc = torch.tensor((mean_train_acc)/len(train_dataloader), dtype=torch.float)
@@ -215,7 +210,7 @@ def run(args, train, sparse_evidences, claims_dict):
         best_accuracy = torch.tensor(max(train_acc.cpu().numpy(), best_accuracy.cpu().numpy()))
         is_best = bool(train_acc >= best_accuracy)
         
-        save_checkpoint({"epoch": epoch, "state_dict": model.state_dict(), "best_accuracy": best_accuracy}, is_best, filename=model_checkpoint_dir + "accuracy_{}".format(train_acc.cpu().numpy()))
+        putils.save_checkpoint({"epoch": epoch, "state_dict": model.state_dict(), "best_accuracy": best_accuracy}, is_best, filename="accuracy_{}".format(train_acc.cpu().numpy()) + model_checkpoint_dir)
 
 if __name__=="__main__":
     args = parse_args()
@@ -236,7 +231,7 @@ if __name__=="__main__":
         claims_dict
     except:
         print("Loading claims data...")
-        claims_dict = joblib.load("new_claims_dict.pkl")
+        claims_dict = joblib.load("claims_dict.pkl")
 
     torch.multiprocessing.set_start_method("fork", force=True)
     run(args, train, sparse_evidences, claims_dict)
