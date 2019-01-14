9907323d26bafd755bbcec87acf75d3f3a1d6edd
Moin Nadeem <mnadeem@mit.edu>
2019-01-14 16:32:03 -0500

    honestly so much

--- sorted_eval.py
+++ sorted_eval.py
@@ -45,7 +45,7 @@ nltk.data.path.append('/usr/users/mnadeem/nltk_data/')
 def parse_args():
     parser = argparse.ArgumentParser(description='Learning the optimal convolution for network.')
     parser.add_argument("--batch-size", type=int, help="Number of queries per batch.", default=1)
-    parser.add_argument("--data-batch-size", type=int, help="Number of examples per query.", default=8)
+    parser.add_argument("--data-sampling", type=int, help="Number of examples per query.", default=8)
     parser.add_argument("--learning-rate", type=float, help="Learning rate for model.", default=1e-3)
     parser.add_argument("--epochs", type=int, help="Number of epochs to learn for.", default=3)
     parser.add_argument("--randomize", default=False, action="store_true")
@@ -58,7 +58,7 @@ def parse_args():
 def run():
     BATCH_SIZE = args.batch_size
     LEARNING_RATE = args.learning_rate
-    DATA_BATCH_SIZE = args.data_batch_size
+    DATA_SAMPLING = args.data_sampling
     NUM_EPOCHS = args.epochs
     MODEL = args.model
     RANDOMIZE = args.randomize
@@ -78,12 +78,12 @@ def run():
     model.load_state_dict(torch.load(MODEL))
 
     print("Created dataset...")
-    dataset = pytorch_data_loader.WikiDataset(test, claims_dict, data_batch_size=DATA_BATCH_SIZE, testFile="shared_task_dev.jsonl", sparse_evidences=sparse_evidences, randomize=RANDOMIZE) 
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=True, collate_fn=pytorch_data_loader.PadCollate())
+    dataset = pytorch_data_loader.ValWikiDataset(test, claims_dict, testFile="shared_task_dev.jsonl", sparse_evidences=sparse_evidences, batch_size=BATCH_SIZE) 
+    dataloader = DataLoader(dataset, num_workers=0, collate_fn=pytorch_data_loader.PadCollate())
 
-    OUTPUT_FREQ = int((len(dataset)/BATCH_SIZE)*0.02) 
+    OUTPUT_FREQ = int((len(dataset))*0.10) 
     
-    parameters = {"batch size": BATCH_SIZE, "data batch size": DATA_BATCH_SIZE, "data": args.data}
+    parameters = {"batch size": BATCH_SIZE, "data sampling rate": DATA_SAMPLING, "data": args.data}
     exp_params = {}
     exp = Experiment("CLSM V2")
     for key, value in parameters.items():
@@ -95,7 +95,7 @@ def run():
     test_running_accuracy = 0.0
     test_running_recall_at_ten = 0.0
 
-    recall_intervals = [1,2,5,10,20,40,60,80,100,200,400]
+    recall_intervals = [1,2,5,10,20,40,60,80,100,200,250,300,400]
     recall = {}
     for i in recall_intervals:
         recall[i] = []
@@ -105,18 +105,16 @@ def run():
     print("Evaluating...")
     beginning_time = time.time() 
 
+    prev_claim = None
     for batch_num, inputs in enumerate(dataloader):
         num_batches += 1
         claims_tensors, claims_text, evidences_tensors, evidences_text, labels = inputs  
 
-        #claims_tensors = claims_tensors.cuda()
-        #evidences_tensors = evidences_tensors.cuda()
-        #labels = labels.cuda()
+        claims_tensors = claims_tensors.cuda()
+        evidences_tensors = evidences_tensors.cuda()
+        labels = labels.cuda()
 
-        try:
-            y_pred = model(claims_tensors, evidences_tensors)
-        except:
-            print(claims_text, evidences_text)
+        y_pred = model(claims_tensors, evidences_tensors)
 
         y = (labels).float()
 
@@ -127,32 +125,58 @@ def run():
         y = y.view(-1)
         y_pred = y_pred.view(-1)
 
-        bin_acc = torch.sigmoid(y_pred).cuda()
-        sorted_idxs = torch.sort(bin_acc, descending=True)[1]
+        bin_acc = torch.sigmoid(y_pred).to("cuda")
 
-        # for idx in range(len(y)):
-          # print("Claim: {}, Evidence: {}, Prediction: {}, Label: {}".format(claims_text[idx], evidences_text[idx], bin_acc[idx], y[idx])) 
+        if prev_claim is None:
+            all_y = y 
+            all_evidences = evidences_text 
+            all_bin_acc = bin_acc 
+        elif prev_claim!=claims_text[0]:
+            # handle ranking here!
+            sorted_idxs = torch.sort(all_bin_acc, descending=True)[1]
+
+            relevant_evidences = []
+            for idx in range(all_y.shape[0]):
+                try:
+                    if int(all_y[idx]):
+                        relevant_evidences.append(all_evidences[idx])
+                except Exception as e:
+                    print(all_y, all_y[idx], idx)
+                    raise e
+
+            # if len(relevant_evidences)==0:
+                # print("Zero relevant", y.sum())
+
+            retrieved_evidences = []
+            for idx in sorted_idxs:
+                retrieved_evidences.append(all_evidences[idx])
+
+            for k in recall_intervals:
+                if len(relevant_evidences)==0:
+                    recall[k].append(0)
+                else:
+                    recall[k].append(calculate_recall(retrieved_evidences, relevant_evidences, k=k))
+
+            if len(relevant_evidences)==0:
+                test_running_recall_at_ten += 0.0
+            else:
+                test_running_recall_at_ten += calculate_recall(retrieved_evidences, relevant_evidences, k=50)
+
+            # reset tensors
+            all_y = y 
+            all_evidences = evidences_text 
+            all_bin_acc = bin_acc 
+        else:
+            all_bin_acc = torch.cat([all_bin_acc, bin_acc])
+            all_evidences.extend(evidences_text)
+            all_y = torch.cat([all_y, y]) 
+        prev_claim = claims_text[0]
+
+        for idx in range(len(y)):
+          print("Claim: {}, Evidence: {}, Prediction: {}, Label: {}".format(claims_text[idx], evidences_text[idx], bin_acc[idx], y[idx])) 
         
         # compute recall
         # assuming only one claim, this creates a list of all relevant evidences
-
-        relevant_evidences = []
-        for idx in range(len(y)):
-            if y[idx]:
-                relevant_evidences.append(evidences_text[idx])
-
-        if len(relevant_evidences)==0:
-            print("Zero relevant", y.sum())
-
-        retrieved_evidences = []
-        for idx in sorted_idxs:
-            retrieved_evidences.append(evidences_text[idx])
-
-        for k in recall_intervals:
-            recall[k].append(calculate_recall(retrieved_evidences, relevant_evidences, k=k))
-
-        test_running_recall_at_ten += calculate_recall(retrieved_evidences, relevant_evidences, k=20)
-
         y = y.round()
         bin_acc = bin_acc.round()
         true.extend(y.tolist())
@@ -164,7 +188,9 @@ def run():
 
         if batch_num % OUTPUT_FREQ==0 and batch_num>0:
             elapsed_time = time.time() - beginning_time
-            print("[{}:{:3f}s]: accuracy: {}, recall@20: {}".format(batch_num, elapsed_time, test_running_accuracy / OUTPUT_FREQ, test_running_recall_at_ten / OUTPUT_FREQ))
+            print("[{}:{:3f}s]: accuracy: {}, recall@50: {}".format(batch_num, elapsed_time, test_running_accuracy / OUTPUT_FREQ, test_running_recall_at_ten / OUTPUT_FREQ))
+            for k, v in recall.items():
+                print("recall@{}: {}".format(k, np.mean(v)))
 
             # 1. Log scalar values (scalar summary)
             info = { 'test_accuracy': test_running_accuracy/OUTPUT_FREQ }
@@ -202,7 +228,11 @@ def run():
 
     filename = "predicted_labels/predicted_labels"
     for key, value in parameters.items():
-        filename += "_{}-{}".format(key.replace(" ", "_"), value)
+        key = key.replace(" ", "_")
+        key = key.replace("/", "_")
+        if type(value)==str:
+            value = value.replace("/", "_")
+        filename += "_{}-{}".format(key, value)
 
     joblib.dump({"true": true, "pred": pred}, filename)
 
@@ -246,6 +276,15 @@ if __name__=="__main__":
     except:
         print("Loading validation claims data...")
         claims_dict = joblib.load("claims_dict.pkl")
+
+    test[2576]['evidence'] = test[0]['evidence']
+    test[4176]['evidence'] = test[0]['evidence']
+    test[4936]['evidence'] = test[0]['evidence']
+    test[9835]['evidence'] = test[0]['evidence']
+    test[10857]['evidence'] = test[0]['evidence']
+    test[12177]['evidence'] = test[0]['evidence']
+    test[12478]['evidence'] = test[0]['evidence']
+    test[14404]['evidence'] = test[0]['evidence']
 
     torch.multiprocessing.set_start_method("spawn", force=True)
     run()
