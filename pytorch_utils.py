b26f382c4e8673c092f1ec0ee3cf7e4bb217de0a
Moin Nadeem <mnadeem@mit.edu>
2019-01-21 17:54:26 -0500

    Added contrastive loss

--- pytorch_utils.py
+++ pytorch_utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -10,3 +11,43 @@ def save_checkpoint(state, is_best, filename='/output/checkpoint.pth.tar'):
         torch.save(state['model'], filename)  # save checkpoint
     else:
         print ("=> Validation Accuracy did not improve")
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+
+    Based on: 
+    """
+
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def check_type_forward(self, in_types):
+        assert len(in_types) == 3
+
+        x0_type, x1_type, y_type = in_types
+        assert x0_type.size() == x1_type.shape
+        assert x1_type.size()[0] == y_type.shape[0]
+        assert x1_type.size()[0] > 0
+        assert x0_type.dim() == 2
+        assert x1_type.dim() == 2
+        assert y_type.dim() == 1
+
+    def check_types(self, in_types):
+        assert len(in_types)==2
+
+    def forward(self, diff, y):
+        # self.check_type_forward((x0, x1, y))
+        self.check_types((diff, y))
+
+        # euclidian distance
+        # diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / dist.size()[0]
+        return loss
